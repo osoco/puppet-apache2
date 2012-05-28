f08afeedba681f541e8e29473b3809b268293bde
deigote <diego@toharia.com>
2012-05-28 16:47:49 +0200

    Added missing dollar sign to var declaration

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -1,8 +1,8 @@
 define apache2::proxy($virtualhost_name, $server_admin_mail, $is_https = false, $ssl_cert = "", $ssl_cert_key = "", 
-                      $backend_url, $proxy_location = "/", proxy_type = "http", bypass_locations = []) {
+                      $backend_url, $proxy_location = "/", $proxy_type = "http", $bypass_locations = []) {
 
     apache2::module { "$virtualhost_name-proxy-$proxy_type-module":
-        modname => "proxy_${proxy_type}"
+        modname => "proxy_$proxy_type"
     }
 
     apache2::virtualhost { "$virtualhost_name":
