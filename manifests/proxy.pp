c4e0b581d655dbe61fac3d3910e3a90d5f23fefb
deigote <diego@toharia.com>
2012-05-28 17:51:21 +0200

    More than one same type proxy caused dupe resources, fixed

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -1,8 +1,11 @@
 define apache2::proxy($virtualhost_name, $server_admin_mail, $is_https = false, $ssl_cert = "", $ssl_cert_key = "", 
                       $backend_url, $proxy_location = "/", $proxy_type = "http", $bypass_locations = []) {
 
-    apache2::module { "$virtualhost_name-proxy-$proxy_type-module":
-        modname => "proxy_$proxy_type"
+    $mod_name = "proxy_$proxy_type"
+    if !defined(Apache2::Module["$mod_name"]) {
+        apache2::module { "apache2-module-$mod_name":
+            modname => "$mod_name"
+        }
     }
 
     apache2::virtualhost { "$virtualhost_name":
