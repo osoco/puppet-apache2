364c2b07f8d83b68856744afcb6fece6a15d458e
deigote <diego@toharia.com>
2012-05-30 17:54:53 +0200

    Some distros doesnt grant apache2 user with permissions to his own home..

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,14 @@
 class apache2 ($version = "latest", $apache2_user = 'www-data') {
 
     package { "apache2":
-        ensure => $version
+        ensure => $version,
+        notify => Exec['grant-home-access-to-apache2-user']
+    }
+
+    exec { 'grant-home-access-to-apache2-user':
+       command => "chown $apache2_user:$apache2_user ~$apache2_user",
+       require => Package['apache2'],
+       refreshonly => true
     }
 
     service { "apache2":
